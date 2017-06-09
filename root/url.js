c3faf43c5a4e3146550bc3a3b8c8abfa18a89bba
Leonard Allain-Launay <leonard@codingame.com>
2017-06-09 15:01:59 -0700

    Shorter stub for URL

--- root/url.js
+++ root/url.js
@@ -2,27 +2,25 @@ const URL = require('url').URL;
 
 const myURL = new URL('https://example.org:8080/foo?first=john&last=doe#bar');
 
-console.log('### Properties');
-
-console.log('href: ', myURL.href);
-console.log('protocol: ', myURL.protocol);
-console.log('pathname: ', myURL.pathname);
-console.log('host: ', myURL.host);
-console.log('port: ', myURL.port);
-console.log('hostname: ', myURL.hostname);
-console.log('hash: ', myURL.hash);
-console.log('search: ', myURL.search);
+console.log(`## Properties:
+  href:     ${myURL.href}
+  protocol: ${myURL.protocol}
+  pathname: ${myURL.pathname}
+  host:     ${myURL.host}
+  port:     ${myURL.port}
+  hostname: ${myURL.hostname}
+  hash:     ${myURL.hash}
+  search:   ${myURL.search}
+`);
 
 // You can also set these properties
 myURL.port = 9999;
-console.log('New port: ', myURL.port);
+console.log('New port:', myURL.port);
 
 // Inspecting search parameters
-
-console.log('### Search Parameters');
-
+console.log('## Search Parameters:');
 const searchParams = myURL.searchParams;
 
-searchParams.forEach((value, name, searchParams) => {
+searchParams.forEach((value, name) =>
   console.log(name, ':', value);
-});
+);
