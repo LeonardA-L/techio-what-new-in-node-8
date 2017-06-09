eb5541b7f1996a3266f8258e49b3da2fb48d3b7c
Leonard Allain-Launay <leonard@codingame.com>
2017-06-09 14:54:37 -0700

    Shorter stub for URL

--- root/url.js
+++ root/url.js
@@ -5,19 +5,12 @@ const myURL = new URL('https://example.org:8080/foo?first=john&last=doe#bar');
 console.log('### Properties');
 
 console.log('href: ', myURL.href);
-
 console.log('protocol: ', myURL.protocol);
-
 console.log('pathname: ', myURL.pathname);
-
 console.log('host: ', myURL.host);
-
 console.log('port: ', myURL.port);
-
 console.log('hostname: ', myURL.hostname);
-
 console.log('hash: ', myURL.hash);
-
 console.log('search: ', myURL.search);
 
 // You can also set these properties
